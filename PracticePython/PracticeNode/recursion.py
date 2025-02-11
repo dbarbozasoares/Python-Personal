2ed7657036ebc687fe81ffb794e6cecf0665ce7c
dbarbozasoares <dbarboza.soares@gmail.com>
2025-02-11 11:17:46 -0500

    Happy day

--- PracticePython/PracticeNode/recursion.py
+++ PracticePython/PracticeNode/recursion.py
@@ -1,7 +1,67 @@
 
-i = 1
+def getTuple():
+    people = [
+        ('Alice', 30),
+        ('Bob', 25),
+        ('Charlie', 35),
+        ('David', 40),
+        ('Eve', 22),
+        ('Frank', 28),
+        ('Grace', 32),
+        ('Hannah', 24),
+        ('Isaac', 29),
+        ('Jack', 34),
+        ('Karen', 27),
+        ('Liam', 31),
+        ('Mia', 33),
+        ('Nathan', 26),
+        ('Olivia', 38),
+        ('Paul', 39),
+        ('Quincy', 41),
+        ('Rita', 23),
+        ('Sam', 36),
+        ('Tina', 37),
+        ('Ursula', 42),
+        ('Victor', 45),
+        ('Wendy', 50),
+        ('Xander', 33),
+        ('Yara', 28),
+        ('Zane', 26),
+    ]
+    return people
+
+def getList():
+    people = {
+    'Alice': 30,
+    'Bob': 25,
+    'Charlie': 35,
+    'David': 40,
+    'Eve': 22,
+    'Frank': 28,
+    'Grace': 32,
+    'Hannah': 24,
+    'Isaac': 29,
+    'Jack': 34,
+    'Karen': 27,
+    'Liam': 31,
+    'Mia': 33,
+    'Nathan': 26,
+    'Olivia': 38,
+    'Paul': 39,
+    'Quincy': 41,
+    'Rita': 23,
+    'Sam': 36,
+    'Tina': 37,
+    'Ursula': 42,
+    'Victor': 45,
+    'Wendy': 50,
+    'Xander': 33,
+    'Yara': 28,
+    'Zane': 26,
+    }
+    return people
+
 def func(num):
-    global i
     if num == 1:
         print("printing 1")
         return 1
@@ -17,7 +77,6 @@ def func(num):
         print("%2 == 0 : ", res*res)
         return res+res
     else:
-        
         print("Result sum: ", res)
         return res+ res
     
@@ -39,10 +98,36 @@ def func3(string,a,b):
         return True
     else:
         func3(string,a+1,b)
-        
+
+#T(n) = 1 + 1 + 1 * n 
+#T(n) = 3 * n
+#T(n) is O(n)    
+def findPeopleUsingTuple(people, size):
+    if size == 0:
+        return
+    else: #1
+        if people[size][1] < 30:#1
+            print(people[size]) 
+    findPeople(people,size-1) # 1 * n
+ 
+def findPeopleUsingHash(people, size):
+    if size == 0:
+        return
+    else:
+        name = list(people.keys())[size]
+        age = people[name]
+        if age < 30:
+            print(name, people[name])
+    findPeopleUsingHash(people, size-1)
+ 
 def main():
-    name = "Doego"
-    func3(name, 0,len(name)-1)
+    people = getList()
+    findPeopleUsingHash(people, len(people)-1)
     
 if __name__ == "__main__":
     main()
+    
+    
+    
+    
+    
